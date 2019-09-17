dbc2805dbe13808f9c66bd9450672e6c11558e19
RASHIDA BENGALI <bengali.rashida@gmail.com>
2019-09-17 10:37:17 +0000

    added a test for fun facts

--- client/src/js/pages/reminderPage/reminderPage.spec.js
+++ client/src/js/pages/reminderPage/reminderPage.spec.js
@@ -12,6 +12,10 @@ describe('ReminderPage', () => {
       const page = new ReminderPage();
       expect(page.render()).toContain("<h1>Interesting Fun Facts</h1>");
     });
+    it('should contain fun facts', () => {
+      const page = new ReminderPage();
+      expect(page.render()).toContain("Gravity was found by Newton");
+    });
   });
 
 });
