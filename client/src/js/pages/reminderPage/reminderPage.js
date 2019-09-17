b437c4f41f4b6510bd521454152ebecaa0449eae
RASHIDA BENGALI <bengali.rashida@gmail.com>
2019-09-17 10:33:40 +0000

    created skeleton of reminder page

---
+++ client/src/js/pages/reminderPage/reminderPage.js
@@ -0,0 +1,6 @@
+const BasePage = require('watch-framework').BasePage;
+class ReminderPage extends BasePage {
+  template = require('./reminderPage.hbs');
+}
+
+module.exports = ReminderPage;
---
+++ client/src/js/pages/reminderPage/reminderPage.spec.js
@@ -0,0 +1,17 @@
+const ReminderPage = require('./reminderPage');
+
+describe('ReminderPage', () => {
+  let watchFace;
+  beforeEach(() => {
+    document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
+    watchFace = document.getElementById('watch-face');
+  });
+
+  describe('#render', () => {
+    it('should render my reminder page', () => {
+      const page = new ReminderPage();
+      expect(page.render()).toContain("<h1>Interesting Fun Facts</h1>");
+    });
+  });
+
+});
