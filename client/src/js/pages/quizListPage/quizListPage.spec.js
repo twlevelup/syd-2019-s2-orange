504a63624405d7fb4521b149a01c2f78ce39866a
Hayden Fuller <hayden.fuller@gmail.com>
2019-10-01 09:41:33 +0000

    Added the quizz and multiple choice

--- client/src/js/pages/quizListPage/quizListPage.spec.js
+++ client/src/js/pages/quizListPage/quizListPage.spec.js
@@ -5,7 +5,13 @@ describe('QuizListPage', () => {
   describe('contents to appear once the page renders', () => {
     it('should contain at lease 1 quiz', () => {
       const page = new QuizListPage();
-      expect(page.render()).toContain('Quiz 1');
+      expect(page.render()).toContain('Which of these animals is not in the feline family?');
+      expect(page.render()).toContain('Tiger');
+      expect(page.render()).toContain('Leopard');
+      expect(page.render()).toContain('Hyena');
+      expect(page.render()).toContain('Jaguar');
+
+
     });
   });
 
