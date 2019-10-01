80870e31e0a19a9135edc9aeab2423d2c1d2399e
Hayden Fuller <hayden.fuller@gmail.com>
2019-10-01 09:50:58 +0000

    fixed broken build and removed bullet points from quiz page

--- client/src/js/pages/categoryPage/categoryPage.spec.js
+++ client/src/js/pages/categoryPage/categoryPage.spec.js
@@ -3,13 +3,10 @@ const CategoryPage = require('./categoryPage');
 describe('CategoryPage', () => {
 
   describe('contents to appear once the page renders', () => {
-    it('should render 4 categories and a title', () => {
+    it('should render 1 categories and a title', () => {
       const page = new CategoryPage();
-      expect(page.render()).toContain("<p> Science </p>");
-      expect(page.render()).toContain("<p> Technology </p>");
-      expect(page.render()).toContain("<p> Engineering </p>");
-      expect(page.render()).toContain("<p> Mathematics </p>");
       expect(page.render()).toContain("<h1>Games</h1>");
+      expect(page.render()).toContain("<p> Science </p>");
     });
   });
 
