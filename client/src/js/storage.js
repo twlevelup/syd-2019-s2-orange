5ae9fc60360b9b66828d452c727846933ff263a9
Viola Hu <lovelyviolaat@hotmail.com>
2019-10-19 03:41:39 +0000

    [Viola/Natalia][Quiz] default rewards page score from zero

--- client/src/js/storage.js
+++ client/src/js/storage.js
@@ -2,4 +2,4 @@ const StorageHub = require('watch-framework').StorageHub;
 const data = require('../../resources/menu.json');
 StorageHub.setJSON(data);
 StorageHub.setData('hello', 'world')
-StorageHub.setData('STEM', 25);
+StorageHub.setData('STEM', 0);
