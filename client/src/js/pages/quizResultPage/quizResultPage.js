65d57a44a03118088d58894a7575095fda44779d
Viola Hu <lovelyviolaat@hotmail.com>
2019-10-19 00:18:56 +0000

    [Viola/Natalia][Quiz] Render correct message on quiz result page

--- client/src/js/pages/quizResultPage/quizResultPage.js
+++ client/src/js/pages/quizResultPage/quizResultPage.js
@@ -1,9 +1,19 @@
 const BasePage = require('watch-framework').BasePage;
+const StorageHub = require('watch-framework').StorageHub;
+
 
 class QuizResultPage extends BasePage {
 
   template = require('./quizResultPage.hbs');
 
+  pageWillLoad() {
+    const result = StorageHub.getData('quizResult');
+
+    this.message = 'Congratulations!';
+    if (result === false) {
+      this.message = 'Better luck next time!';
+    }
+  }
 }
 
 module.exports = QuizResultPage;
--- client/src/js/pages/quizResultPage/quizResultPage.spec.js
+++ client/src/js/pages/quizResultPage/quizResultPage.spec.js
@@ -1,12 +1,22 @@
 const QuizResult = require('./quizResultPage');
+const StorageHub = require('watch-framework').StorageHub;
 
 describe('QuizResultPage', () => {
-
   describe('contents to appear once the page renders', () => {
-    it('should render congratulations', () => {
+    it('should render congratulations when quizResult is true', () => {
       const page = new QuizResult();
+
+      StorageHub.setData('quizResult', true);
+      page.pageWillLoad();
       expect(page.render()).toContain("<h1>Congratulations!</h1>");
     });
-  });
 
+    it('should render wrong message when quizResult is false in storage', () => {
+      const page = new QuizResult();
+
+      StorageHub.setData('quizResult', false);
+      page.pageWillLoad();
+      expect(page.render()).toContain("<h1>Better luck next time!</h1>");
+    });
+  });
 });
